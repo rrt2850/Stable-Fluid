97b0bcd78e04ddea3957351a94ef4d5fae7b5246
rrt2850 <79067594+rrt2850@users.noreply.github.com>
2026-02-17 01:43:14 -0500

    Improve runtime defaults and compile classes into out directory

--- start.ps1
+++ start.ps1
@@ -2,10 +2,15 @@ param(
     [int]$GridWidth = 128,
     [int]$GridHeight = 128,
     [int]$EmitterCount = 8,
-    [int]$SimulationSteps = 180
+    [int]$SimulationSteps = 180,
+    [bool]$ExportVideo = $false,
+    [bool]$LogEveryStep = $false
 )
 
 $ErrorActionPreference = 'Stop'
 
-javac src/*.java
-java -cp src Main $GridWidth $GridHeight $EmitterCount $SimulationSteps
+$BuildDir = 'out/classes'
+New-Item -ItemType Directory -Force -Path $BuildDir | Out-Null
+
+javac -d $BuildDir src/*.java
+java -cp $BuildDir Main $GridWidth $GridHeight $EmitterCount $SimulationSteps $ExportVideo $LogEveryStep
--- start.sh
+++ start.sh
@@ -5,6 +5,11 @@ GRID_WIDTH="${1:-128}"
 GRID_HEIGHT="${2:-128}"
 EMITTER_COUNT="${3:-8}"
 SIMULATION_STEPS="${4:-180}"
+EXPORT_VIDEO="${5:-false}"
+LOG_EVERY_STEP="${6:-false}"
 
-javac src/*.java
-java -cp src Main "$GRID_WIDTH" "$GRID_HEIGHT" "$EMITTER_COUNT" "$SIMULATION_STEPS"
+BUILD_DIR="out/classes"
+mkdir -p "$BUILD_DIR"
+
+javac -d "$BUILD_DIR" src/*.java
+java -cp "$BUILD_DIR" Main "$GRID_WIDTH" "$GRID_HEIGHT" "$EMITTER_COUNT" "$SIMULATION_STEPS" "$EXPORT_VIDEO" "$LOG_EVERY_STEP"
