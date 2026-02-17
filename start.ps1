fdb098ac5034978aef566833c4eff53134edbca4
rrt2850 <79067594+rrt2850@users.noreply.github.com>
2026-02-17 01:15:24 -0500

    Add MP4 diffusion export and faster default emitters

--- start.ps1
+++ start.ps1
@@ -1,10 +1,11 @@
 param(
     [int]$GridWidth = 128,
     [int]$GridHeight = 128,
-    [int]$EmitterCount = 8
+    [int]$EmitterCount = 8,
+    [int]$SimulationSteps = 180
 )
 
 $ErrorActionPreference = 'Stop'
 
 javac src/*.java
-java -cp src Main $GridWidth $GridHeight $EmitterCount
+java -cp src Main $GridWidth $GridHeight $EmitterCount $SimulationSteps
--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 GRID_WIDTH="${1:-128}"
 GRID_HEIGHT="${2:-128}"
 EMITTER_COUNT="${3:-8}"
+SIMULATION_STEPS="${4:-180}"
 
 javac src/*.java
-java -cp src Main "$GRID_WIDTH" "$GRID_HEIGHT" "$EMITTER_COUNT"
+java -cp src Main "$GRID_WIDTH" "$GRID_HEIGHT" "$EMITTER_COUNT" "$SIMULATION_STEPS"
